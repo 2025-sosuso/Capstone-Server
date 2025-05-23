50f0333385e43c3fa44d90c0e6ffc7aa944a3815
강예린 <yerin7786@gmail.com>
2025-05-23 23:00:17 +0900

    feat: JWT 기반 인증 필터 구현

---
+++ src/main/java/com/knu/sosuso/capstone/jwt/JwtFilter.java
@@ -0,0 +1,73 @@
+package com.knu.sosuso.capstone.jwt;
+
+import com.knu.sosuso.capstone.dto.CustomOAuth2User;
+import com.knu.sosuso.capstone.dto.request.AuthRequest;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String authorization = null;
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            System.out.println(cookie.getName());
+            if (cookie.getName().equals("Authorization")) {
+                authorization = cookie.getValue();
+            }
+        }
+        if (authorization == null) {
+            System.out.println("token null");
+            filterChain.doFilter(request, response);
+
+            return;
+        }
+
+        // 토큰
+        String token = authorization;
+
+        // 토큰 소멸 시간 검증
+        if (jwtUtil.isExpired(token)) {
+            System.out.println("token expired");
+            filterChain.doFilter(request, response);
+
+            // 조건이 해당되면 메서드 종료 (필수)
+            return;
+        }
+
+        // 토큰에서 user 정보 획득
+        String sub = jwtUtil.getSub(token);
+        String email = jwtUtil.getEmail(token);
+        String name = jwtUtil.getName(token);
+        String role = jwtUtil.getRole(token);
+        String picture = jwtUtil.getPicture(token);
+
+        // userDto를 생성하여 값 set
+        AuthRequest authRequest = new AuthRequest(sub, email, name, role, picture);
+
+        // UserDetails에 회원 정보 객체 담기
+        CustomOAuth2User customOAuth2User = new CustomOAuth2User(authRequest);
+
+        // 스프링 시큐리티 인증 토큰 생성
+        Authentication authToken = new UsernamePasswordAuthenticationToken(customOAuth2User, null, customOAuth2User.getAuthorities());
+        // 세션에 사용자 등록
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+
+        filterChain.doFilter(request, response);
+    }
+}
