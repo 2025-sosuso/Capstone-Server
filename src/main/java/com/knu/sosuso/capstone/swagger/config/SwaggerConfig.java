010d3e3130769e198892d1d2390b660bf787af74
강예린 <yerin7786@gmail.com>
2025-06-07 18:57:34 +0900

    fix: Swagger에 HTTPS 서버 설정 추가

--- src/main/java/com/knu/sosuso/capstone/swagger/config/SwaggerConfig.java
+++ src/main/java/com/knu/sosuso/capstone/swagger/config/SwaggerConfig.java
@@ -1,25 +1,30 @@
 package com.knu.sosuso.capstone.swagger.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Capstone API",
+                version = "1.0.0",
+                description = "우리들의 API들 .. ^0^"
+        ),
+        servers = {
+                @Server(url = "https://knu-sosuso.com", description = "Production server"),
+                @Server(url = "http://localhost:8080", description = "Local development server")
+        }
+)
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
-    }
-
-    private Info apiInfo() {
-        return new Info()
-                .title("Capstone API")
-                .description("우리들의 API들 .. ^0^")
-                .version("1.0.0");
+                .components(new Components());
     }
 }
