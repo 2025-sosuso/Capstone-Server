a0740490649ec7be55dbbb73f12072e24d1b5cc9
강예린 <yerin7786@gmail.com>
2025-06-09 02:43:29 +0900

    feat: Scrap 예외 추가

--- src/main/java/com/knu/sosuso/capstone/exception/error/ScrapError.java
+++ src/main/java/com/knu/sosuso/capstone/exception/error/ScrapError.java
@@ -6,7 +6,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ScrapError implements BaseError {
 
-    SCRAP_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 스크랩된 영상입니다.");
+    SCRAP_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 스크랩된 영상입니다."),
+    SCRAP_NOT_FOUNT(HttpStatus.NOT_FOUND, "존재하지 않는 스크랩입니다."),
+    FORBIDDEN_SCRAP_DELETE(HttpStatus.FORBIDDEN, "본인의 스크랩만 취소할 수 있습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
