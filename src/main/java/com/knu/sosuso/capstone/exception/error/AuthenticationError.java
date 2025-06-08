a345435b2780108cb7ad675780fcbb59fbe4eb0c
강예린 <yerin7786@gmail.com>
2025-06-09 03:44:50 +0900

    refactor: INVALID_TOKEN의 HttpStatus 변경

--- src/main/java/com/knu/sosuso/capstone/exception/error/AuthenticationError.java
+++ src/main/java/com/knu/sosuso/capstone/exception/error/AuthenticationError.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum AuthenticationError implements BaseError {
 
-    INVALID_TOKEN(HttpStatus.BAD_REQUEST, "유효하지 않은 토큰입니다."),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다.");
 
     private final HttpStatus httpStatus;
