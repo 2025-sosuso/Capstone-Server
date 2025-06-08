b5544f241c9ffc82bce9fb70cc3068fe90fb639f
강예린 <yerin7786@gmail.com>
2025-06-08 14:47:44 +0900

    feat: 관심 채널 등록 API Swagger 적용

---
+++ src/main/java/com/knu/sosuso/capstone/swagger/FavoriteChannelControllerSwagger.java
@@ -0,0 +1,35 @@
+package com.knu.sosuso.capstone.swagger;
+
+import com.knu.sosuso.capstone.dto.ResponseDto;
+import com.knu.sosuso.capstone.dto.request.RegisterFavoriteChannelRequest;
+import com.knu.sosuso.capstone.dto.response.favorite_channel.RegisterFavoriteChannelResponse;
+import com.knu.sosuso.capstone.swagger.annotation.ErrorCode400;
+import com.knu.sosuso.capstone.swagger.annotation.ErrorCode500;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Tag(name = "관심 채널 API")
+public interface FavoriteChannelControllerSwagger {
+
+    @Operation(
+            summary = "관심 채널 등록",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "관심 채널 등록 성공",
+                            content = @Content(schema = @Schema(implementation = RegisterFavoriteChannelResponse.class))
+                    )
+            }
+    )
+    @ErrorCode400
+    @ErrorCode500
+    ResponseDto<RegisterFavoriteChannelResponse> registerFavoriteChannel(
+            @CookieValue("Authorization") String token,
+            @RequestBody RegisterFavoriteChannelRequest registerFavoriteChannelRequest
+    );
+}
