d714162b2c3e14e754747b4613f61419b87a2c46
강예린 <yerin7786@gmail.com>
2025-06-10 00:34:29 +0900

    feat: 배포 주소로 리다이렉트 되도록 수정 (임시)

--- src/main/java/com/knu/sosuso/capstone/security/CustomSuccessHandler.java
+++ src/main/java/com/knu/sosuso/capstone/security/CustomSuccessHandler.java
@@ -48,7 +48,7 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 
         response.setHeader(HttpHeaders.SET_COOKIE, cookie.toString());
 
-        response.sendRedirect("http://localhost:3000/login/success");
+        response.sendRedirect("https://capstone-client-guka.vercel.app/login/success");
     }
 
     public void logout(String token, HttpServletResponse response) throws IOException {
