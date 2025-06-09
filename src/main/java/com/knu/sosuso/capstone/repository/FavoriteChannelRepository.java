88cd47f0019d03cdbcf242bbe08fd31c568a3a71
강예린 <yerin7786@gmail.com>
2025-06-09 22:33:09 +0900

    Resolving conflicts

--- src/main/java/com/knu/sosuso/capstone/repository/FavoriteChannelRepository.java
+++ src/main/java/com/knu/sosuso/capstone/repository/FavoriteChannelRepository.java
@@ -3,6 +3,11 @@ package com.knu.sosuso.capstone.repository;
 import com.knu.sosuso.capstone.domain.FavoriteChannel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface FavoriteChannelRepository extends JpaRepository<FavoriteChannel, Long> {
     boolean existsByUserIdAndApiChannelId(Long userId, String apiChannelId);
+    Optional<FavoriteChannel> findByUserIdAndApiChannelId(Long userId, String apiChannelId);
+    List<FavoriteChannel> findByUserId(Long userId);
 }
