934cc578281d9ea20ba9111c0a1daf2d7f99db6c
강예린 <yerin7786@gmail.com>
2025-06-12 14:48:59 +0900

    refactor: AI 모델 통신 주소(ngrok) 변경

--- src/main/java/com/knu/sosuso/capstone/ai/service/AnalysisService.java
+++ src/main/java/com/knu/sosuso/capstone/ai/service/AnalysisService.java
@@ -21,7 +21,7 @@ import java.security.cert.X509Certificate;
 public class AnalysisService {
 
     private final RestTemplate restTemplate = new RestTemplate();
-    private static final String FASTAPI_URL = "https://8057-34-143-135-191.ngrok-free.app/analyze";
+    private static final String FASTAPI_URL = "https://d35c-34-124-136-29.ngrok-free.app/analyze";
 
     static {
         try {
