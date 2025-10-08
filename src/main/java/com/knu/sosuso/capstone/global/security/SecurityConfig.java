4c713902600c21312c185378bd9b80d96ce8c2d0
강예린 <yerin7786@gmail.com>
2025-10-08 19:08:01 +0900

    chore: CORS 설정 내 도메인 수정 - 프론트엔드 도메인 주소 수정

--- src/main/java/com/knu/sosuso/capstone/global/security/SecurityConfig.java
+++ src/main/java/com/knu/sosuso/capstone/global/security/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
                 CorsConfiguration configuration = new CorsConfiguration();
                 configuration.setAllowedOriginPatterns(List.of(
                         "http://localhost:3000",
-                        "https://capstone-client.vercel.app",
+                        "https://sosuso-client.vercel.app",
                         "http://localhost:8080",
                         "https://knu-sosuso.com"
                 ));
