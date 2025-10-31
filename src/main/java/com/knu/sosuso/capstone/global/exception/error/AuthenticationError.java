e286f233162cf1abbebccfb75ce99f2b282eb6d6
Alal11 <spalqj56@naver.com>
2025-10-31 23:28:01 +0900

    feat: 예외 처리를 위한 AuthenticationError Enum 추가 #112
    - BusinessException과 통합하여 일관된 예외 처리 가능

--- src/main/java/com/knu/sosuso/capstone/global/exception/error/AuthenticationError.java
+++ src/main/java/com/knu/sosuso/capstone/global/exception/error/AuthenticationError.java
@@ -6,8 +6,11 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum AuthenticationError implements BaseError {
 
+    TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "토큰이 없습니다. 로그인이 필요합니다."),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다.");
+    TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다."),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증되지 않은 사용자입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
