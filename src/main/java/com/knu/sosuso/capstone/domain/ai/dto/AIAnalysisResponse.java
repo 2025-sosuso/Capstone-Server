6fbe179f95823d6dfeab17cb5d42b4fe812603e4
강예린 <yerin7786@gmail.com>
2025-11-09 00:22:36 +0900

    feat: ai 분석 응답 필드에 감정 세분화 데이터 추가

--- src/main/java/com/knu/sosuso/capstone/domain/ai/dto/AIAnalysisResponse.java
+++ src/main/java/com/knu/sosuso/capstone/domain/ai/dto/AIAnalysisResponse.java
@@ -1,6 +1,6 @@
 package com.knu.sosuso.capstone.domain.ai.dto;
 
-import com.knu.sosuso.capstone.domain.comment.entity.value.SentimentType;
+import com.knu.sosuso.capstone.domain.comment.entity.value.CommentSentimentDetail;
 
 import java.util.List;
 import java.util.Map;
@@ -9,9 +9,9 @@ public record AIAnalysisResponse(
         Long videoId, // 영상 videoId
         String apiVideoId, // 영상 apiVideoId
         String summation, // 전체 댓글 요약
-        boolean isWarning,
+        boolean isWarning, // 논란 감지 유무
         List<String> keywords, // 댓글 주요 키워드
-        Map<String, SentimentType> sentimentComments, // 댓글별 긍정, 부정, 기타 (Map<apiCommentId, commentContent>)
+        List<CommentSentimentDetail> sentimentComments, //
         Map<String, Double> languageRatio, // 언어 비율
         Map<String, Double> sentimentRatio // 전체 댓글 긍정, 부정, 기타 비율
 ) {
